e45b0b639fd60cd4db372847b84fef794a55fb67
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-11-22 16:33:30 -0500

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,23 +1,37 @@
 const React = require("react")
+
 const myStyle = {
-    color: "#FFDE00",
-    backgroundColor: "#3B4CCA",
-    textAlign: "center"
+  color: '#FFFFFF',
+  backgroundColor: '#000000',
 };
+
 class Index extends React.Component {
-    render() {
-        const { pokemon } = this.props;    // destructure the pokemon array
-        return (
-            <div style={myStyle}>
-                <h1>See All The Pokemon!</h1>
-                <ul>
-                        {
-                            pokemon.map((eachPokemon, i) => {
-                            })
-                        }
-                    </ul>
-            </div>
-        )
-    }
+  render() {
+      const { pokemon } = this.props
+      return (
+          <div>
+              <h1 style={myStyle}>See All The Pokemon! (INDEX)</h1>
+              <ul>
+                  {
+                  pokemon.map((character, i) => {
+                      let characterName = character.name
+                      return (
+                    <li>
+                      {/* key = {i} */}
+                      The{' '}
+                      <a href={`/pokemon/${i}`}>
+                      {/* charAt(0).toUpperCase() = allow us to capitalize the first letter of the word */}
+                 {
+                 characterName.charAt(0).toUpperCase() + characterName.slice(1).toLowerCase()
+                 }
+                      </a>
+                    </li>
+                      )
+                  })
+                  }
+              </ul>
+          </div>
+      )
+  }
 }
-module.exports = Index;
+module.exports = Index
