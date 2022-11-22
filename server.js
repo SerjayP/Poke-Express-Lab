611dab35eff1fa888503c6cfe5e3a4bcdbdb8bac
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-11-22 16:29:22 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const reactViews = require("express-react-views");
 const app = express()
 const PORT = 3000
 const pokemon = require("./models/pokemon")
 
+
+app.set("view engine", "jsx")
+app.engine("jsx", reactViews.createEngine());
+
 app.get('/pokemon', (req, res)=>{
-    res.send(pokemon)
+    res.render("Index", {pokemon: pokemon })
 })
 
 app.listen(PORT, ()=>{
---
+++ views/Index.jsx
@@ -0,0 +1,23 @@
+const React = require("react")
+const myStyle = {
+    color: "#FFDE00",
+    backgroundColor: "#3B4CCA",
+    textAlign: "center"
+};
+class Index extends React.Component {
+    render() {
+        const { pokemon } = this.props;    // destructure the pokemon array
+        return (
+            <div style={myStyle}>
+                <h1>See All The Pokemon!</h1>
+                <ul>
+                        {
+                            pokemon.map((eachPokemon, i) => {
+                            })
+                        }
+                    </ul>
+            </div>
+        )
+    }
+}
+module.exports = Index;
